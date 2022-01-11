fad87f9567c8e4d87e0e90ac853e6877f369b760
咩屁啊 <zhou.zhangtao@yalla.live>
2022-01-11 14:18:59 +0800

    feat: 初始化项目

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  "plugins": [
+    [
+      "import",
+      {
+        "libraryName": "@formily/antdv",
+        "libraryDirectory": "esm",
+        "style": true
+      },
+      "@formily/antdv"
+    ]
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormProvider :form="form">
+      <SchemaField :schema="schema" />
+    </FormProvider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Input, FormItem } from "@formily/antdv";
+import { FormProvider, createSchemaField } from "@formily/vue";
+import { createForm } from "@formily/core";
+
+const { SchemaField } = createSchemaField({
+  components: {
+    Input,
+    FormItem
+  },
+});
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FormProvider,
+    SchemaField,
+  },
+  data() {
+    const form = createForm({ editable: false });
+    const schema = {
+      type: "object",
+      properties: {
+        Input: {
+          type: "string",
+          title: "Input",
+          "x-decorator": "FormItem",
+          "x-component": "Input",
+          "x-validator": [],
+          "x-component-props": {},
+          "x-decorator-props": {},
+          name: "Input",
+          "x-designable-id": "8zq561r4ebq",
+          "x-index": 0,
+        },
+      },
+      "x-designable-id": "83u6b8yund8",
+    };
+    return {
+      form,
+      schema,
+    };
+  },
+  mounted() {
+    this.form.setValues({
+      Input: 123
+    });
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
